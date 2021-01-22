a93ccf0a4a2cb2a2fd8484939cb8c1bf494ecbb8
leoFullStackDeveloper <leo.technology@hotmail.com>
2021-01-22 12:07:19 -0500

    multiply and divide

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
 
+
     public int sumar(int num1, int num2){
         return num1 + num2;
     }
@@ -7,6 +8,17 @@ public class Calculator {
     public int restar(int num1, int num2){
         return num1 - num2;
     }
+    public int multiplicar(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public double  dividir(double num1, double num2){
+        if (num2 == 0){
+            return 0;
+        }else{
+            return num1 / num2;
+        }
+
+    }
 
 }
-
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
         Calculator calc1 = new Calculator();
 
         System.out.println("La suma de 25 + 15 es =  " + calc1.sumar(25,15));
-        System.out.println("La suma de 25 - 15 es =  " + calc1.restar(25,15));
+        System.out.println("La resta de 25 - 15 es =  " + calc1.restar(25,15));
+        System.out.println("La multiplicación de 25 - 15 es =  " + calc1.multiplicar(25,15));
+        System.out.println("La division de 25 - 0 es =  " + calc1.dividir(25,0));
     }
 
 }
