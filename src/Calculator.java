aef7b3335b596d71249361dca1823eb3fdb4d136
leoFullStackDeveloper <leo.technology@hotmail.com>
2021-01-22 12:03:29 -0500

    add and subtract methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,12 @@
 public class Calculator {
+
+    public int sumar(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int restar(int num1, int num2){
+        return num1 - num2;
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello calculator");
+
+        Calculator calc1 = new Calculator();
+
+        System.out.println("La suma de 25 + 15 es =  " + calc1.sumar(25,15));
+        System.out.println("La suma de 25 - 15 es =  " + calc1.restar(25,15));
+    }
+
 }
